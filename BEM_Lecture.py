3c567fdfb880b4763791b961af98882a4aec34f1
C43353 <30357693+C43353@users.noreply.github.com>
2023-03-05 11:41:47 +0000

    Add files via upload

--- BEM_Lecture.py
+++ BEM_Lecture.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  4 18:55:18 2023
+Created on Sun Mar  5 10:49:42 2023
 
 @author: C43353
 """
@@ -9,8 +9,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
 
+from BEM_Functions_1.0 import nodal
+
 """
 BEM Using CLD from lectures folder to match with lectures data
+Changing calculations to use functions instead
 code has -
 Constants:
     angular velocity
@@ -95,50 +98,8 @@ for n, V0 in enumerate(speeds):
     tau = []
 
     for r in segments:
-        aa = 0.0  # Induction Factor
-        ar = 0.0  # Angular Induction Factor
 
-        xi = (omega * r) / V0  # Local Velocity Ratio
-        s = (c * B) / (2 * np.pi * r)  # Solidity
-
-        for i in range(100):
-            phi = np.arctan((1 - aa) / ((1 + ar) * xi))  # Relative Wind Angle
-
-            alpha = phi * (180 / np.pi) - theta  # Angle of Attack
-
-            Cl = float(fcl(alpha))  # Lift Coefficient
-            Cd = float(fcd(alpha))  # Drag Coefficient
-
-            Cn = Cl * np.cos(phi) + Cd * np.sin(phi)  # Normal Coefficient
-            Cr = Cl * np.sin(phi) - Cd * np.cos(phi)  # Tangent Coefficient
-
-            # Prandtl Loss Factor
-            F = (2 / np.pi) * np.arccos(np.exp(- (
-                B * (1 - (r / R))) / (2 * (r / R) * np.sin(phi) * r)))
-
-            K = (4 * F * (np.sin(phi) ** 2)) / (s * Cn)  # Useful Coefficient
-
-            aa = 1 / (K + 1)  # Calc New Induction Factor
-            # From Lecture Calculation
-            if aa > ac:
-                aa = 1 - ((K * (1 - (2 * ac))) / 2) * (
-                    np.sqrt(1 + (4 / K) * (((1 - ac) / (
-                        1 - (2 * ac))) ** 2)) - 1)
-
-            # From Converting Matlab Code
-            # if aa > ac:
-                # aa = 1 + np.sqrt(
-                # 1 + (4 / K) * (((1 - ac) / (1 - (2 * ac))) ** 2))
-                # aa = 1 - (K * ((1 - (2 * ac)) / (2)))
-
-            ar = 1 / ((4 * np.sin(phi) * np.cos(phi)) / (s * Cr) - 1)
-
-        # Relative Wind Speed (Both equations near identical output)
-        Vrel = ((1-aa)/(np.sin(phi))) * V0
-        # Vrel = ((1 + ar) / (np.cos(phi))) * omega * r
-
-        fn = (1 / 2) * Cn * rho * (Vrel ** 2) * c  # Normal Force at Node
-        fr = (1/2) * Cr * rho * (Vrel ** 2) * c  # Rotational Force at Node
+        
 
         # Final Output Values
         phi_list.append(phi)
